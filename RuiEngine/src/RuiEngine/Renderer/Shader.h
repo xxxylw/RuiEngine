329a23c378d8d316f3687d4d7ba5505a0bfe7f04
HuangQiRui <xxxylww@163.com>
2025-10-23 19:33:40 +0800

    Update : Moved OpenGL shader implementation to OpenGLShader class + added more uniform functions

--- RuiEngine/src/RuiEngine/Renderer/Shader.h
+++ RuiEngine/src/RuiEngine/Renderer/Shader.h
@@ -7,14 +7,12 @@ namespace RuiEngine {
 	class Shader
 	{
 	public:
-		Shader(const std::string& vertexSrc, const std::string& fragmentSrc);
-		~Shader();
+		virtual ~Shader() = default;
 
-		void Bind();
-		void UnBind();
+		virtual void Bind() const = 0;
+		virtual void UnBind() const = 0;
 
-		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
-
+		static Shader* Create(const std::string& vertexSrc, const std::string& fragmentSrc);
 	private:
 		uint32_t m_RendererId;
 	};
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,9 @@
 #include "RuiEngine.h"
 
-#include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include "Platform/OpenGL/OpenGLShader.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "imgui/imgui.h"
 
@@ -95,7 +93,7 @@ public:
 			}
 		)";
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -112,21 +110,23 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(v_Position, 1.0f);
+				color = vec4(u_Color, 1.0f);
 			}
 		)";
 
-		m_Shader.reset(new RuiEngine::Shader(vertexSrc, fragmentSrc));
-		m_BlueShader.reset(new RuiEngine::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_Shader.reset(RuiEngine::Shader::Create(vertexSrc, fragmentSrc));
+		m_FlatColorShader.reset(RuiEngine::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 		/* End Draw */
 	}
 
@@ -157,13 +157,17 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<RuiEngine::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<RuiEngine::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				RuiEngine::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				RuiEngine::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -178,8 +182,8 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-		ImGui::Begin("Test");
-		ImGui::Text("Hello World");
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
 		ImGui::End();
 	}
 
@@ -187,7 +191,7 @@ public:
 		std::shared_ptr<RuiEngine::Shader> m_Shader;
 		std::shared_ptr<RuiEngine::VertexArray> m_VertexArray;
 		
-		std::shared_ptr<RuiEngine::Shader> m_BlueShader;
+		std::shared_ptr<RuiEngine::Shader> m_FlatColorShader;
 		std::shared_ptr<RuiEngine::VertexArray> m_SquareVA;
 
 		RuiEngine::OrthographicCamera m_Camera;
@@ -196,6 +200,8 @@ public:
 		
 		float m_CameraRotation = 0.0f;
 		float m_CameraRotationSpeed = 180.0f;
+
+		glm::vec3 m_SquareColor = { 0.2f, 0.4f, 0.8f };
 };
 
 class Sandbox : public RuiEngine::Application {
