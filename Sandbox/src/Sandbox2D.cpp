af016e2abd08bcca9f793e590630f6a19ded9073
HuangQiRui <xxxylww@163.com>
2025-11-02 14:50:38 +0800

    WIP: before creating one_hour_game branch

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -41,7 +41,7 @@ void Sandbox2D::OnUpdate(RuiEngine::Timestep ts)
 		RE_PROFILE_SCOPE("Renderer Draw");
 		RuiEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
 		RuiEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 0.5f, 0.5f }, m_SquareColor);
-		RuiEngine::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 1.0f }, { 0.5f, 0.8f, 0.3f, 1.0f });
+		RuiEngine::Renderer2D::DrawQuad({ 0.5f, -0.5f, -0.4f }, { 0.5f, 1.0f }, { 0.5f, 0.8f, 0.3f, 1.0f });
 		RuiEngine::Renderer2D::DrawRotatedQuad({ 1.0f, 1.0f, -0.5f }, { 10.0f, 10.0f }, glm::radians(45.0f), m_MaxVer);
 		RuiEngine::Renderer::EndScene();
 	}
