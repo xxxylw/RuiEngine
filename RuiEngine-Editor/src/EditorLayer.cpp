3cfdc437b618b292563934b7d3808c924f66bbae
HuangQiRui <xxxylww@163.com>
2025-11-09 16:06:02 +0800

    Fix : Fix RuiEngine-Editor bug when viewport resized

--- RuiEngine-Editor/src/EditorLayer.cpp
+++ RuiEngine-Editor/src/EditorLayer.cpp
@@ -32,6 +32,15 @@ namespace RuiEngine {
 	{
 		RE_PROFILE_FUNCTION();
 
+		// Resize
+		if (RuiEngine::FramebufferSpecification spec = m_Framebuffer->GetSpecification();
+			m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f && // zero sized framebuffer is invalid
+			(spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
+		{
+			m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+			m_CameraController.OnResize(m_ViewportSize.x, m_ViewportSize.y);
+		}
+
 		// Update
 		if(m_ViewportFocused)
 			m_CameraController.OnUpdate(ts);
@@ -153,13 +162,7 @@ namespace RuiEngine {
 		Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused || !m_ViewportHovered);
 
 		ImVec2 viewPortPanelSize = ImGui::GetContentRegionAvail();
-		if (m_ViewportSize != *(glm::vec2*)&viewPortPanelSize)
-		{
-			m_ViewportSize = { viewPortPanelSize.x, viewPortPanelSize.y };
-			m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
-
-			m_CameraController.OnResize(viewPortPanelSize.x, viewPortPanelSize.y);
-		}
+		m_ViewportSize = { viewPortPanelSize.x, viewPortPanelSize.y };
 		uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
 		ImGui::Image((void*)textureID, ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
 		ImGui::End();
--- RuiEngine/src/RuiEngine/Core/Core.h
+++ RuiEngine/src/RuiEngine/Core/Core.h
@@ -1,9 +1,40 @@
 #pragma once
 
-#include "memory"
+#include <memory>
+
+#ifdef _WIN32
+	#ifdef _WIN64
+		#define RE_PLATFORM_WINDOWS
+	#endif // _WIN64
+	#else
+	#error "x86 Builds are not supported!"
+#endif // _WIN32
+// End of platform detection
+
+#ifdef RE_DEBUG
+	#if defined(RE_PLATFORM_WINDOWS)
+		#define RE_DEBUGBREAK() __debugbreak()
+	#else
+	#error "Platform doesn't support debugbreak yet!"
+	#endif
+
+	#define RE_ENABLE_ASSERTS
+#else
+	#define RE_DEBUGBREAK()
+#endif // RE_DEBUG
+
+#ifdef RE_ENABLE_ASSERTS
+	#define RE_ASSERT(x, ...) { if(!(x)) { RE_ERROR("Assertion Failed: {0}", __VA_ARGS__); RE_DEBUGBREAK(); } }
+	#define RE_CORE_ASSERT(x, ...) { if(!(x)) { RE_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); RE_DEBUGBREAK(); } }
+#else
+	#define RE_ASSERT(x, ...)
+	#define RE_CORE_ASSERT(x, ...)
+#endif // RE_ENABLE_ASSERTS
+
+
 
 #ifdef RE_PLATFORM_WINDOWS
-	#if HZ_DYNAMIC_LINK
+	#if RE_DYNAMIC_LINK
 		#ifdef RE_BUILD_DLL
 			#define RE_API __declspec(dllexport)
 		#else
@@ -11,26 +42,10 @@
 		#endif // RE_BUILD_DLL
 	#else
 		#define RE_API
-	#endif // HZ_DYNAMIC_LINK
+	#endif // RE_DYNAMIC_LINK
 #else
 	#error RuiEngine only supports Windows!
 #endif
-
-#ifdef RE_DEBUG
-	#define RE_ENABLE_ASSERTS
-#endif // RE_DEBUG
-
-
-#ifdef RE_ENABLE_ASSERTS
-	#define RE_ASSERT(x, ...) {if(!(x))\
-		{RE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak();}}
-	#define RE_CORE_ASSERT(x, ...) {if(!(x))\
-		{ RE_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); }}
-#else
-	#define RE_ASSERT(x, ...)
-	#define RE_CORE_ASSERT(x, ...)
-#endif // RE_ENABLE_ASSERTS
-
 
 #define BIT(x) (1 << x)
 #define RE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
