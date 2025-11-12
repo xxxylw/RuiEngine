0e1447f60bc75243b8dedb43716c4175cb8ddacd
HuangQiRui <xxxylww@163.com>
2025-11-12 18:29:46 +0800

    Fixed : BeginScene shoud not input mainCamera->Proj as params because it would construct a Camera as input

--- RuiEngine/src/RuiEngine/Scene/Scene.cpp
+++ RuiEngine/src/RuiEngine/Scene/Scene.cpp
@@ -94,7 +94,7 @@ namespace RuiEngine {
 
 		if (mainCamera)
 		{
-			Renderer2D::BeginScene(mainCamera->GetProjection(), *cameraTransform);
+			Renderer2D::BeginScene(*mainCamera, *cameraTransform);
 
 			auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
 			for (auto entity : group)
