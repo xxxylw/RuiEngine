3a1fc21cc49e4dd3a24bef9d5377f2e73bcb60fc
HuangQiRui <xxxylww@163.com>
2025-12-10 13:25:18 +0800

    Update : Added physics collider visualization

--- RuiEngine-Editor/src/EditorLayer.h
+++ RuiEngine-Editor/src/EditorLayer.h
@@ -24,6 +24,8 @@ namespace RuiEngine {
 		bool OnKeyPressed(KeyPressedEvent& e);
 		bool OnMouseButtonPressed(MouseButtonPressedEvent& e);
 
+		void OnOverlayRender();
+
 		void NewScene();
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
@@ -71,6 +73,8 @@ namespace RuiEngine {
 		glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 
 		uint32_t m_GizmoType = -1;
+
+		bool m_ShowPhysicsColliders = false;
 
 		enum class SceneState
 		{
